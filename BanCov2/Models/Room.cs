ae0a00d156722976fa20c9fa9ecc19e546eb7ab2
THANH DAT DO <dothanhdat12012003@gmail.com>
2024-10-16 15:31:51 +0700

    add room

---
+++ BanCov2/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System;
+
+@Entity
+public class Room
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomName;
+    private String creator;
+
+    @ElementCollection
+    private List<String> players = new ArrayList<>(); // Danh sách người chơi
+
+    // Getters và setters
+}
